1a8c5e3e5cf23d811b43485cfba973972ca8ae42
BobIvans <130351128+BobIvans@users.noreply.github.com>
2026-07-20 23:10:53 +0300

    PR-033: export detector primitives

--- src/strategy/__init__.py
+++ src/strategy/__init__.py
@@ -1,4 +1,5 @@
 """Strategy runtime package."""
+from .detectors import CircularArbitrageDetector, DetectorPair
 from .domain import Opportunity
 from .interfaces import Strategy, StrategyMode, StrategyContext
 from .registry import StrategyRegistry
@@ -6,6 +7,14 @@ from .runtime import StrategyRuntime, TaskSupervisor
 from .queue import OpportunityQueue
 
 __all__ = [
-    "Opportunity", "Strategy", "StrategyMode", "StrategyContext",
-    "StrategyRegistry", "StrategyRuntime", "TaskSupervisor", "OpportunityQueue",
+    "Opportunity",
+    "Strategy",
+    "StrategyMode",
+    "StrategyContext",
+    "StrategyRegistry",
+    "StrategyRuntime",
+    "TaskSupervisor",
+    "OpportunityQueue",
+    "CircularArbitrageDetector",
+    "DetectorPair",
 ]

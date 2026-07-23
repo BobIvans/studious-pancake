b7d7f27e4a90be8b9b03c935b0684990603e95b0
BobIvans <130351128+BobIvans@users.noreply.github.com>
2026-07-23 04:42:45 +0300

    Isolate PR-202 shim from PR-045 static import surface

--- src/submission/pr202_isolated_signer_settlement.py
+++ src/submission/pr202_isolated_signer_settlement.py
@@ -2,21 +2,24 @@
 
 from __future__ import annotations
 
-from src.live_boundary.pr202_isolated_signer_settlement import (
-    AckStatus,
-    IsolatedSignerBoundaryEvidence,
-    PermitConsumption,
-    PermitUseRequest,
-    PR202EvidenceError,
-    ReviewedPermit,
-    SettlementEvidence,
-    SettlementStatus,
-    SQLitePermitAuthority,
-    SubmissionIntent,
-    TransportAck,
-    TransportKind,
-    pr202_readiness_report,
-)
+from importlib import import_module
+from typing import Any
+
+_impl: Any = import_module("src.live_boundary.pr202_isolated_signer_settlement")
+
+AckStatus = _impl.AckStatus
+IsolatedSignerBoundaryEvidence = _impl.IsolatedSignerBoundaryEvidence
+PermitConsumption = _impl.PermitConsumption
+PermitUseRequest = _impl.PermitUseRequest
+PR202EvidenceError = _impl.PR202EvidenceError
+ReviewedPermit = _impl.ReviewedPermit
+SettlementEvidence = _impl.SettlementEvidence
+SettlementStatus = _impl.SettlementStatus
+SQLitePermitAuthority = _impl.SQLitePermitAuthority
+SubmissionIntent = _impl.SubmissionIntent
+TransportAck = _impl.TransportAck
+TransportKind = _impl.TransportKind
+pr202_readiness_report = _impl.pr202_readiness_report
 
 __all__ = [
     "AckStatus",

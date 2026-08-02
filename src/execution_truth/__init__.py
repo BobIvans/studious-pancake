bd3d7305d33dcff6f0ced8e731fcb940df9c80b0
BobIvans <130351128+BobIvans@users.noreply.github.com>
2026-08-02 21:08:14 +0300

    feat(mpr-sys-02): add execution truth package surface

---
+++ src/execution_truth/__init__.py
@@ -0,0 +1,37 @@
+"""Canonical MPR-SYS-02 execution-truth authority."""
+
+from .runtime import (
+    CompiledMessageRef,
+    DurableAttemptRef,
+    EXECUTION_TRUTH_EVIDENCE_SCHEMA_ID,
+    EXECUTION_TRUTH_SCHEMA_ID,
+    ExecutionStage,
+    ExecutionTruthBundle,
+    ExecutionTruthError,
+    PlanRef,
+    ReconciliationRef,
+    RootedCandidateRef,
+    SimulationRef,
+    TerminalState,
+    evaluate_bundle,
+    terminalize_ambiguous,
+    validate_transition,
+)
+
+__all__ = [
+    "CompiledMessageRef",
+    "DurableAttemptRef",
+    "EXECUTION_TRUTH_EVIDENCE_SCHEMA_ID",
+    "EXECUTION_TRUTH_SCHEMA_ID",
+    "ExecutionStage",
+    "ExecutionTruthBundle",
+    "ExecutionTruthError",
+    "PlanRef",
+    "ReconciliationRef",
+    "RootedCandidateRef",
+    "SimulationRef",
+    "TerminalState",
+    "evaluate_bundle",
+    "terminalize_ambiguous",
+    "validate_transition",
+]

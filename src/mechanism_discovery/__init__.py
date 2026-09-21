80c4829f435d6e04893689781590e400082a97cf
BobIvans <130351128+BobIvans@users.noreply.github.com>
2026-09-21 07:02:31 +0300

    PR-354: add src/mechanism_discovery/__init__.py

---
+++ src/mechanism_discovery/__init__.py
@@ -0,0 +1,11 @@
+"""PR-354 mechanism-discovery research package."""
+
+from .manifest import ALL_FUNCTIONS, FUNCTION_COUNT, NF_IDS, NF_TO_SYMBOL, PACKAGES
+
+__all__ = [
+    "ALL_FUNCTIONS",
+    "FUNCTION_COUNT",
+    "NF_IDS",
+    "NF_TO_SYMBOL",
+    "PACKAGES",
+]

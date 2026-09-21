944e9e6f9f1c872db7e753c24a90de43a60e7be4
BobIvans <130351128+BobIvans@users.noreply.github.com>
2026-09-21 05:03:26 +0300

    MEGA8-08 residual: expose exact ownership manifest

---
+++ src/mega8_08/__init__.py
@@ -0,0 +1,3 @@
+"""MEGA8-08 residual owners recovered by ULTIMATE-MEGA1 closure."""
+from .manifest import ALL_FUNCTIONS, CHILDREN, FUNCTION_COUNT, NF_IDS
+__all__ = ["ALL_FUNCTIONS", "CHILDREN", "FUNCTION_COUNT", "NF_IDS"]
